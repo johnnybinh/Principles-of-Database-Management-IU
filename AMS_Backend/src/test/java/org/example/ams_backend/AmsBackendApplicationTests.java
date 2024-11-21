e068b55e203c152aca9d15dbccbc8e1c470f0dae
DrGnNatsu <phphuong.career@gmail.com>
2024-11-21 20:34:02 +0700

    Phuong | Feat: Init the backend

---
+++ AMS_Backend/src/test/java/org/example/ams_backend/AmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ams_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
