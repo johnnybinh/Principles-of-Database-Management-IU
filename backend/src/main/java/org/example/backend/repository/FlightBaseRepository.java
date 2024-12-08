31f35774f90347617b9de7decc5fae67c33bdc53
DrGnNatsu <phphuong.career@gmail.com>
2024-12-08 09:06:57 +0700

    Phuong | Finish

--- backend/src/main/java/org/example/backend/repository/FlightBaseRepository.java
+++ backend/src/main/java/org/example/backend/repository/FlightBaseRepository.java
@@ -13,10 +13,10 @@ public interface FlightBaseRepository extends JpaRepository<FlightBase, Long> {
     @Query("SELECT fb FROM FlightBase fb " +
             "JOIN fb.flightSchedule fs " +
             "JOIN fb.airport a " +
-            "WHERE (:departure IS NULL OR a.city = :departure) " +
-            "AND (:arrival IS NULL OR a.city = :arrival) " +
-            "AND (:departureDate IS NULL OR fs.departureDate = :departureDate) " +
-            "AND (:arrivalDate IS NULL OR fs.arrivalDate = :arrivalDate)")
+            "WHERE (:departure IS NULL OR fs.departure LIKE %:departure%) " +
+            "AND (:arrival IS NULL OR fs.arrival LIKE %:arrival%) " +
+            "AND (:departureDate IS NULL OR DATE(fs.departureDate) = DATE(:departureDate)) " +
+            "AND (:arrivalDate IS NULL OR DATE(fs.arrivalDate) = DATE(:arrivalDate))")
     List<FlightBase> searchFlights(
             @Param("departure") String departure,
             @Param("arrival") String arrival,
